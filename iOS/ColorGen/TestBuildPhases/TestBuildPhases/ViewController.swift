5ff2a963b3725c94266bf4b88c841ca3b05a034c
Alvin <ling94alvin@gmail.com>
2020-06-14 14:32:38 -0500

    Add test project for run script

---
+++ iOS/ColorGen/TestBuildPhases/TestBuildPhases/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBuildPhases
+//
+//  Created by Alvin Ling on 6/14/20.
+//  Copyright © 2020 Alvin Ling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
