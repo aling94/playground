a97546560dff930f3b8b9e559881a7ee0bea8782
Alvin <ling94alvin@gmail.com>
2020-08-02 15:52:17 -0500

    HTTP methods

--- iOS/Services.playground/Contents.swift
+++ iOS/Services.playground/Contents.swift
@@ -1,39 +1,65 @@
 import UIKit
 
 enum Configuration: String {
-    case dev, qa, stage, prod
+    case dev, qa, stage, release
 }
 
 protocol Application {
     var config: Configuration { get }
     var version: Decimal { get }
     var build: UInt { get }
-    var net: Network { get }
+    var env: Environment { get }
 }
 
-protocol Network {
+protocol Environment {
     var serviceURL: URL { get }
     var contentURL: URL { get }
 }
 
-protocol NetClient {
+protocol WebClient {
+    typealias Callback<T> = (Result<T, Error>, URLResponse?) -> Void
     var session: URLSession { get }
-    var baseURL: URL { get }
+    func request(from url: URL) -> URLRequest
 }
 
-extension NetClient {
-
-    func GET<P: Encodable, T: Decodable>(
-        _ parameters: P? = nil,
-        _ completion: @escaping (Result<T, Error>) -> Void) {
-
-        session.dataTask(with: URLRequest(url: baseURL)) { (data, response, error) in
-
-        }
-
-
+extension URLRequest {
+    enum Method: String {
+        case GET, POST, DELETE
     }
 
+    var method: Method? {
+        get { Method(rawValue: httpMethod ?? "") }
+        set { httpMethod = newValue?.rawValue }
+    }
+}
+
+extension WebClient {
+
+    private func serveRequest<T: Decodable>(_ request: URLRequest, _ method: URLRequest.Method, _ completion: @escaping Callback<T>) {
+        var request = request
+        request.method = method
+        session.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                if let error = error { completion(.failure(error), response) }
+                return
+            }
+            do { completion(.success(try JSONDecoder().decode(T.self, from: data)), response) }
+            catch { completion(.failure(error), response) }
+        }.resume()
+    }
+
+    func GET<T: Decodable>(_ url: URL, _ completion: @escaping Callback<T>) {
+        GET(request(from: url), completion)
+    }
+
+    func GET<T: Decodable>(_ request: URLRequest, _ completion: @escaping Callback<T>) {
+        serveRequest(request, .GET, completion)
+    }
+}
+
+protocol WebResource {
+    var client: WebClient { get }
+    var baseURL: URL { get }
 }
 
 protocol PathChain {
@@ -54,14 +80,16 @@ extension PathChain where Self: RawRepresentable, RawValue == String {
 }
 
 enum Services: PathChain { typealias Parent = Services
+    enum User: PathChain { typealias Parent = Services }
+}
 
-    enum User: PathChain { typealias Parent = Services
+extension Services.User {
+    enum Document: String, PathChain {
+        typealias Parent = Services.User
 
-        enum Document: String, PathChain { typealias Parent = User
-            case upload
-            case download = "downloading"
-            case create
-        }
+        case upload
+        case download = "downloading"
+        case create
     }
 }
 
@@ -69,7 +97,6 @@ var components = URLComponents()
 components.scheme = "https"
 components.host = "api.github.com"
 components.path = Services.User.Document.download.path
-components.port
 print(components.url!)
 
 extension URL {
